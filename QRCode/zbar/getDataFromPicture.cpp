6967c9c4b54d2ee2fa278b34cffe9b4d1741b967
ruritoBlogger <rurito.blog@gmail.com>
2020-08-10 22:49:25 +0900

    zbarを用いた場合を追加

---
+++ QRCode/zbar/getDataFromPicture.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <string>
+#include <opencv2/opencv.hpp>
+#include <zbar.h>
+
+int main()
+{
+    // opencvのカメラの初期化
+    cv::VideoCapture cap("./qr.png");
+
+    if(!cap.isOpened())
+    {
+      std::cout << "cannot read camera" << std::endl;
+      return -1;
+    }
+
+    zbar::ImageScanner scanner;
+    // disable all
+    scanner.set_config(zbar::ZBAR_NONE, zbar::ZBAR_CFG_ENABLE, 0);
+
+    // enable qr
+    scanner.set_config(zbar::ZBAR_QRCODE, zbar::ZBAR_CFG_ENABLE, 1);
+    cv::Mat frame;
+    
+    // カメラから情報を読み込み続ける
+    while(cap.read(frame))
+    {
+	cv::cvtColor(frame, frame, cv::COLOR_BGR2GRAY);
+        cv::imshow("real time",frame);
+
+	zbar::Image image(frame.rows, frame.cols, "Y800", frame.data, frame.rows*frame.cols);
+	
+	int n = scanner.scan(image);
+	
+	  // Print results
+	  for(zbar::Image::SymbolIterator symbol = image.symbol_begin(); symbol != image.symbol_end(); ++symbol)
+	  {
+	    std::cout << "Data : " << symbol->get_data() << std::endl;
+	  }
+
+	//auto result = ReadBarcode({frame.data, frame.rows, frame.cols, ImageFormat::RGBX}, hints);
+	//std::cout << "data is " << TextUtfEncoding::ToUtf8(result.text()) << std::endl;	
+        //qボタンが押されたとき処理を終了する
+        const int key = cv::waitKey(1);
+        if(key == 'q') break;
+    }
+    cv::destroyAllWindows();
+}
---
+++ QRCode/zxing/getDataFromCamera.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <string>
+#include <opencv2/opencv.hpp>
+#include <ZXing/ReadBarcode.h>
+#include <ZXing/TextUtfEncoding.h>
+#include <ZXing/BarcodeFormat.h>
+#include <ZXing/DecodeStatus.h>
+#define STB_IMAGE_IMPLEMENTATION
+#include "./thirdparty/stb/stb_image.h"
+
+using namespace ZXing;
+
+int main()
+{
+    // opencvのカメラの初期化
+    cv::VideoCapture cap(0);
+
+    if(!cap.isOpened())
+    {
+      std::cout << "cannot read camera" << std::endl;
+      return -1;
+    }
+
+    // QRCodeのDecoderの設定
+    DecodeHints hints;
+    hints.setTryHarder(false);
+
+    cv::Mat frame;
+    
+    // カメラから情報を読み込み続ける
+    while(cap.read(frame))
+    {
+        cv::imshow("real time",frame);
+	
+        void* data = malloc(frame.total() * frame.elemSize());
+	auto result = ReadBarcode({frame.data, frame.rows, frame.cols, ImageFormat::RGBX}, hints);
+	std::cout << "data is " << TextUtfEncoding::ToUtf8(result.text()) << std::endl;	
+        //qボタンが押されたとき処理を終了する
+        const int key = cv::waitKey(1);
+        if(key == 'q') break;
+    }
+    cv::destroyAllWindows();
+}
