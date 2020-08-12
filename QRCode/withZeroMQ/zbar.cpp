81ae64da7000b0cad77281f216fd21b8a9e29ae8
rurito <rurito.blog@gmail.com>
2020-08-12 16:11:01 +0900

    update

--- QRCode/withZeroMQ/zbar.cpp
+++ QRCode/withZeroMQ/zbar.cpp
@@ -2,47 +2,67 @@
 #include <string>
 #include <opencv2/opencv.hpp>
 #include <zbar.h>
+#include "zmq_addon.hpp"
+
+void my_free(void *data, void *hint)
+{
+        free(data);
+}
 
 int main()
 {
-    // opencvのカメラの初期化
-    cv::VideoCapture cap(0);
-
-    if(!cap.isOpened())
-    {
-      std::cout << "cannot read camera" << std::endl;
-      return -1;
-    }
-
+    // zbarの初期設定
     zbar::ImageScanner scanner;
     // disable all
     scanner.set_config(zbar::ZBAR_NONE, zbar::ZBAR_CFG_ENABLE, 0);
 
     // enable qr
     scanner.set_config(zbar::ZBAR_QRCODE, zbar::ZBAR_CFG_ENABLE, 1);
-    cv::Mat frame;
-    
-    // カメラから情報を読み込み続ける
-    while(cap.read(frame))
+
+    zmq::context_t ctx;
+    zmq::socket_t socket(ctx, zmq::socket_type::pull);
+    socket.bind("tcp://*:11000");
+
+    zmq::message_t rcv_msg;
+    int rows, cols, type;
+    cv::Mat img;
+    void *data;
+
+    while(true)
     {
-	cv::cvtColor(frame, frame, cv::COLOR_BGR2GRAY);
-        cv::imshow("real time",frame);
+        //heightの受信
+        socket.recv(&rcv_msg, 0);
+        rows = *(int*)rcv_msg.data();
 
-	zbar::Image image(frame.cols, frame.rows, "Y800", frame.data, frame.cols*frame.rows);
-	
-	int n = scanner.scan(image);
-	
-	  // Print results
-	  for(zbar::Image::SymbolIterator symbol = image.symbol_begin(); symbol != image.symbol_end(); ++symbol)
-	  {
-	    std::cout << "Data : " << symbol->get_data() << std::endl;
-	  }
+        //widthの受信
+        socket.recv(&rcv_msg, 0);
+        cols = *(int*)rcv_msg.data();
 
-	//auto result = ReadBarcode({frame.data, frame.rows, frame.cols, ImageFormat::RGBX}, hints);
-	//std::cout << "data is " << TextUtfEncoding::ToUtf8(result.text()) << std::endl;	
-        //qボタンが押されたとき処理を終了する
-        const int key = cv::waitKey(1);
-        if(key == 'q') break;
+        //chの受信
+        socket.recv(&rcv_msg, 0);
+        type = *(int*)rcv_msg.data();
+        
+        //データの受信
+        socket.recv(&rcv_msg, 0);
+        data = (void*)rcv_msg.data();
+        printf("rows=%d, cols=%d type=%d\n", rows, cols, type);
+
+        if (type == 2) {
+            img = cv::Mat(rows, cols, CV_8UC1, data);
+        }
+        else {
+            img = cv::Mat(rows, cols, CV_8UC3, data);
+        }
+	    cv::cvtColor(img, img, cv::COLOR_BGR2GRAY);
+	    zbar::Image image(img.cols, img.rows, "Y800", img.data, img.cols*img.rows);
+	
+	    int n = scanner.scan(image);
+	
+	    // Print results
+	    for(zbar::Image::SymbolIterator symbol = image.symbol_begin(); symbol != image.symbol_end(); ++symbol)
+	    {
+	        std::cout << "Data : " << symbol->get_data() << std::endl;
+	    }
     }
     cv::destroyAllWindows();
 }
--- QRCode/withZeroMQ/zxing.cpp
+++ QRCode/withZeroMQ/zxing.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include "zmq_addon.hpp"
 #include <opencv2/opencv.hpp>
 #include <ZXing/ReadBarcode.h>
 #include <ZXing/TextUtfEncoding.h>
@@ -10,34 +11,54 @@
 
 using namespace ZXing;
 
+void my_free(void *data, void *hint)
+{
+        free(data);
+}
+
 int main()
 {
-    // opencvのカメラの初期化
-    cv::VideoCapture cap(0);
-
-    if(!cap.isOpened())
-    {
-      std::cout << "cannot read camera" << std::endl;
-      return -1;
-    }
-
     // QRCodeのDecoderの設定
     DecodeHints hints;
     hints.setTryHarder(false);
 
-    cv::Mat frame;
-    
-    // カメラから情報を読み込み続ける
-    while(cap.read(frame))
+    zmq::context_t ctx;
+    zmq::socket_t socket(ctx, zmq::socket_type::pull);
+    socket.bind("tcp://*:11000");
+
+    zmq::message_t rcv_msg;
+    int rows, cols, type;
+    cv::Mat img;
+    void *data;
+
+    while(true)
     {
-	cv::cvtColor(frame, frame, cv::COLOR_BGR2GRAY);
-        cv::imshow("real time",frame);
-	
-	auto result = ReadBarcode({frame.data, frame.cols, frame.rows, ImageFormat::Lum}, hints);
-	std::cout << "data is " << TextUtfEncoding::ToUtf8(result.text()) << std::endl;	
-        //qボタンが押されたとき処理を終了する
-        const int key = cv::waitKey(1);
-        if(key == 'q') break;
+        //heightの受信
+        socket.recv(&rcv_msg, 0);
+        rows = *(int*)rcv_msg.data();
+
+        //widthの受信
+        socket.recv(&rcv_msg, 0);
+        cols = *(int*)rcv_msg.data();
+
+        //chの受信
+        socket.recv(&rcv_msg, 0);
+        type = *(int*)rcv_msg.data();
+        
+        //データの受信
+        socket.recv(&rcv_msg, 0);
+        data = (void*)rcv_msg.data();
+        printf("rows=%d, cols=%d type=%d\n", rows, cols, type);
+
+        if (type == 2) {
+            img = cv::Mat(rows, cols, CV_8UC1, data);
+        }
+        else {
+            img = cv::Mat(rows, cols, CV_8UC3, data);
+        }
+	    cv::cvtColor(img, img, cv::COLOR_BGR2GRAY);
+	    auto result = ReadBarcode({img.data, img.cols, img.rows, ImageFormat::Lum}, hints);
+	    std::cout << "data is " << TextUtfEncoding::ToUtf8(result.text()) << std::endl;
     }
     cv::destroyAllWindows();
 }
