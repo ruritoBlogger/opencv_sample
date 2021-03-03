9639b76fcbe361598df8b268a0c0ad59666cc613
ruritoBlogger <rurito.blog@gmail.com>
2021-03-04 00:58:46 +0900

    feat: 複数の処理を並行して実行出来るように改修

--- async_zeromq/receiver.py
+++ async_zeromq/receiver.py
@@ -1,9 +1,11 @@
 import asyncio
 import zmq
 import zmq.asyncio
+import time
 
+is_last = False
 
-async def manage_receive(is_last: bool) -> None:
+async def manage_receive() -> None:
     ctx: zmq.asyncio.Context = zmq.asyncio.Context()
     soc: zmq.asyncio.Socket = ctx.socket(zmq.PULL)
     soc.connect("tcp://127.0.0.1:50000")
@@ -18,11 +20,25 @@ async def manage_receive(is_last: bool) -> None:
             msg = await soc.recv_string()
             print(msg)
 
-    while is_last:
+    while not is_last:
         tasks = [wait_message(soc) for i in range(3)]
         await asyncio.wait(tasks)
+
+async def test() -> None:
+    await asyncio.sleep(1)
+    key = 0
+    while True:
+        print("あああああ")
+        await asyncio.sleep(2)
+        key += 1
+
+        if key > 10:
+            is_last = True
+            break
+
 
 if __name__ == "__main__":
     print("START")
     loop = asyncio.get_event_loop()
-    loop.run_until_complete(manage_receive(True))
+    tasks = asyncio.gather(*[manage_receive(), test()])
+    loop.run_until_complete(tasks)
